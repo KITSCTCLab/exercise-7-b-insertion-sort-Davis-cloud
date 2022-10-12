2d8736e8b538bfed00da56a684779fe85aaa7ed0
URK21CS2009 Davis Alex <109649381+Davis-cloud@users.noreply.github.com>
2022-10-12 09:36:03 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
+from typing import List
 
-# data = [9, 5, 1, 4, 3]
-input_data = input()
-data = []
-for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-print(insertionSort(data))
+def insertionSort(array) -> List[int]:
+  for index in range(1, len(array)):
+    key = array[index]    
+    indexo = index - 1
+    while indexo >= 0 and key < array[indexo]:
+      array[indexo + 1] =  array[indexo]
+      indexo -= 1
+    array[indexo + 1] = key
+  return array
